2420ecd76965e9fc7f949fcf51aa8b244b8c7508
Yuri Siman <yurisimannogueira@gmail.com>
2021-01-14 13:35:55 -0300

    Adicionando o contexto do Identity na classe IdentityConfig

--- src/services/VirtualStore.Identidade.API/Configuration/IdentityConfig.cs
+++ src/services/VirtualStore.Identidade.API/Configuration/IdentityConfig.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using VirtualStore.Identidade.API.Data;
 
 namespace VirtualStore.Identidade.API.Configuration
 {
@@ -12,7 +10,8 @@ namespace VirtualStore.Identidade.API.Configuration
     {
         public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
-
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             return services;
         }
