9ad9c6701e79901cf3fa226b2277624c6bbb7de8
Yuri Siman <yurisimannogueira@gmail.com>
2021-01-14 09:00:37 -0300

    Criando classe de configuração da Startup - Configuração da API - ApiConfig

--- src/services/VirtualStore.Identidade.API/Startup.cs
+++ src/services/VirtualStore.Identidade.API/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using VirtualStore.Identidade.API.Configuration;
 
 namespace VirtualStore.Identidade.API
 {
@@ -36,8 +30,8 @@ namespace VirtualStore.Identidade.API
 
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddApiConfiguration();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "VirtualStore.Identidade.API", Version = "v1" });
@@ -46,23 +40,10 @@ namespace VirtualStore.Identidade.API
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "VirtualStore.Identidade.API v1"));
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "VirtualStore.Identidade.API v1"));
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseApiConfiguration(env);
         }
     }
 }
